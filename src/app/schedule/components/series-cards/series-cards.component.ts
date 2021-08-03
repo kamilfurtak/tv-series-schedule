338a9394a4e0623305b063f035fee1f1931bbee7
Kamil Furtak <kf@geomatyka-krakow.pl>
2021-08-03 11:56:46 +0200

    Create empty series cards component #2

---
+++ src/app/schedule/components/series-cards/series-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-series-cards',
+  templateUrl: './series-cards.component.html',
+  styleUrls: ['./series-cards.component.less']
+})
+export class SeriesCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
