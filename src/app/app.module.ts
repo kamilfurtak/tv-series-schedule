c7ee77231aa6d818ea2b0339933196edd784e2a0
Kamil Furtak <kf@geomatyka-krakow.pl>
2021-08-03 10:38:32 +0200

    Use shared module in feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,7 +14,6 @@ import { reducers } from './app.reducer';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
--- src/app/schedule/schedule.module.ts
+++ src/app/schedule/schedule.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { ScheduleComponent } from './schedule.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ScheduleComponent],
-  imports: [CommonModule, ScheduleRoutingModule, NgbDatepickerModule],
+  imports: [SharedModule, ScheduleRoutingModule, NgbDatepickerModule],
 })
 export class ScheduleModule {}
