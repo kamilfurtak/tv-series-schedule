51b99d47b6fc3bc1d62a8429470e80afb5f23f35
Kamil Furtak <kf@geomatyka-krakow.pl>
2021-08-03 11:56:25 +0200

    Create empty series cards component

--- src/app/schedule/schedule.module.ts
+++ src/app/schedule/schedule.module.ts
@@ -8,9 +8,10 @@ import { SharedModule } from '../shared/shared.module';
 import { DatePickerComponent } from './components/date-picker/date-picker.component';
 import { seriesScheduleReducer } from './store/reducers/series-schedule.reducer';
 import { TvSeriesScheduleService } from './services/tv-series-schedule/tv-series-schedule.service';
+import { SeriesCardsComponent } from './components/series-cards/series-cards.component';
 
 @NgModule({
-  declarations: [ScheduleComponent, DatePickerComponent],
+  declarations: [ScheduleComponent, DatePickerComponent, SeriesCardsComponent],
   imports: [
     SharedModule,
     ScheduleRoutingModule,
