ac205e61ac8995aef71aba30d367d82e6f6500d2
Kamil Furtak <kf@geomatyka-krakow.pl>
2021-08-03 09:39:17 +0200

    Scaffold template

--- src/app/schedule/schedule.module.ts
+++ src/app/schedule/schedule.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { ScheduleComponent } from './schedule.component';
 
-
 @NgModule({
-  declarations: [
-    ScheduleComponent
-  ],
-  imports: [
-    CommonModule,
-    ScheduleRoutingModule
-  ]
+  declarations: [ScheduleComponent],
+  imports: [CommonModule, ScheduleRoutingModule, NgbDatepickerModule],
 })
-export class ScheduleModule { }
+export class ScheduleModule {}
