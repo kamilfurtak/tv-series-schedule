f88e265e4af676080079574ada59eaa6f95bd73e
Kamil Furtak <kf@geomatyka-krakow.pl>
2021-08-03 08:24:02 +0200

    Create schedule component

--- src/app/schedule/schedule.module.ts
+++ src/app/schedule/schedule.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ScheduleRoutingModule } from './schedule-routing.module';
+import { ScheduleComponent } from './schedule.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ScheduleComponent
+  ],
   imports: [
     CommonModule,
     ScheduleRoutingModule
